7033c0d50bc0fabb8ec8c0d909ff10a5f776372f
Sofia Medina <medinasofiaa1@gmail.com>
2023-05-11 22:07:36 -0300

    commit actualizado

---
+++ scrip.js
@@ -0,0 +1,16 @@
+const cuerpoPag = document.querySelector("body");
+
+const COLOR_TEXTO = "black";
+const FUENTE_TEXTO = "arial";
+const TAM_TEXTO = "35px";
+const COLOR_FONDO = "white";
+
+
+function modificarPag() {
+
+cuerpoPag.style.color = COLOR_TEXTO;
+cuerpoPag.style.backgroundColor = COLOR_FONDO;
+cuerpoPag.style.fontFamily = FUENTE_TEXTO;
+cuerpoPag.style.fontSize = TAM_TEXTO;
+    
+}
